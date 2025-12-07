09cffa6af661a23dd24e9678686a68773315b6a5
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-08 00:04:53 +0100

    tweaks comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :content, presence: true
-  validates :user_id
-  validates :post_id
+  validates :user_id, numericality: true
+  validates :post_id, numericality: true
   belongs_to :post
   belongs_to :user
 end
